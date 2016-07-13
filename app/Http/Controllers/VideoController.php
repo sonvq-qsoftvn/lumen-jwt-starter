6e33207d8aa86f7cf18a46d9eee620dfd9e64184
sonvq-qsoftvn <vuquangson1610@gmail.com>
2016-07-13 16:47:12 +0700

    commit get video by id

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -9,25 +9,37 @@ class VideoController extends BaseCassandraController {
 
     protected $_keyspace = 'videodb';
     protected $_table = 'videos';
-    
-    public function index() {         
+
+    public function index() {
         $statement = new SimpleStatement(
-            "SELECT * from $this->_table"
+                "SELECT * from $this->_table"
         );
-        
+
         $future = $this->_session->executeAsync($statement);
-        $queryResult = $future->get();             
+        $queryResult = $future->get();
         $result = array();
-        
-        foreach ($queryResult as $row) {                    
+
+        foreach ($queryResult as $row) {
             $result[] = $row;
         }
-        
+
         return [
             'success' => [
                 "$this->_table" => $result,
             ],
         ];
+    }
+
+    public function show($id) {
+        $statement = $this->_session->prepare(
+                "SELECT * FROM $this->_table WHERE videoid = ?"
+        );
+        
+        $queryResult = $this->session->execute($statement, new Cassandra\ExecutionOptions(array(
+            'arguments' => array(new Cassandra\Uuid($id))
+        )));
+        
+        var_dump($queryResult);
     }
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,8 +17,13 @@ $app->group(['prefix' => 'api/v1'], function () use ($app) {
         echo phpinfo();
     });
 
-    $app->get('/videos', 'App\Http\Controllers\VideoController@index');
-
+    $app->get('/videos', ['as' => 'videos_index', 'uses' => 'App\Http\Controllers\VideoController@index']);
+    $app->get('/videos/{id}', ['as' => 'videos_show', 'uses' => 'App\Http\Controllers\VideoController@show']);
+    $app->post('/videos', ['as' => 'videos_store', 'uses' => 'App\Http\Controllers\VideoController@store']);
+    $app->put('/videos/{id}', ['as' => 'videos_update', 'uses' => 'App\Http\Controllers\VideoController@update']);
+    $app->delete('/videos/{id}', ['as' => 'videos_destroy', 'uses' => 'App\Http\Controllers\VideoController@destroy']);
+    $app->delete('/videos', ['as' => 'videos_delete', 'uses' => 'App\Http\Controllers\VideoController@delete']);
+    
     $app->post('/auth/login', 'App\Http\Controllers\Auth\AuthController@postLogin');    
     
 
